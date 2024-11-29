3f7522d1539cd0c1f2d57d292733e9cc0f53233e
MohammedAbuFoul <106703644+MohammedAbuFoul@users.noreply.github.com>
2024-11-29 20:51:57 +0200

    running uvicorn within main
    هذه الطريقة لتشغيل السيرفر عن طريق تشغيل ملف main.py

--- API_Ready_To_Deploy/main.py
+++ API_Ready_To_Deploy/main.py
@@ -44,3 +44,7 @@ def calculate_bmi(
         pic="4"
 
     return BMIOutput(bmi=bmi, message=message,image=pic)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
